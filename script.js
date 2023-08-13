4df7351b8b1a3e940790030c5c9e67d5bf0d650a
Syedsajid7411 <138382056+Syedsajid7411@users.noreply.github.com>
2023-08-13 16:37:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function tec(){
+    document.write("agriculture")
+    document.write("sustain development")
+    document.write("advance development")
+  }
+  
+  function misba(){
+    document.write("tel")
+  }
